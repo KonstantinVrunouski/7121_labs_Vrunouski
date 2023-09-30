71c2992e905a003e1f15a3c1c7fd0f4a4f26f2c4
KonstantinVrunouski <enotsith@gmail.com>
2023-09-30 20:32:35 +0300

    code update final

--- laba3.cpp
+++ laba3.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
-#include <set>
+
+int countDistinct(int arr[], int n)
+{
+    int res = 1;
+ 
+    for (int i = 1; i < n; i++) {
+        int j = 0;
+        for (j = 0; j < i; j++)
+            if (arr[i] == arr[j])
+                break;
+ 
+        if (i == j)
+            res++;
+    }
+    return res;
+}
 
 int main()
 {
-	std::set<int> s;
-	std::multiset<int> s_multi;
-	int cnt = 0, res = 1, n, max_dig = 0, max_dig_index;
+	int cnt = 0, res = 1, n, max_dig = 0, max_dig_index, temp;
 	std::cout << "Input number of the elements of the array:\n";
 	std::cin >> n;
 	int* arr = new int[n];
@@ -13,11 +26,7 @@ int main()
 	for (int i = 0; i < n; ++i)
 	{
 		std::cin >> arr[i];
-		s.insert(arr[i]);
-		s_multi.insert(arr[i]);
 	}
-
-	std::multiset <int>::iterator it = s_multi.begin();
 
 	for (int i = 0; i < n; ++i)
 	{
@@ -40,11 +49,23 @@ int main()
 		res = 0;
 	}
 
-	std::cout << "Number of the unique elements and result of multiplication of the elements after maximum:\n" << s.size() << '\t' << res << '\n' << "Sorted array:\n";
-
-	for (int i = 1; it != s_multi.end(); i++, it++)
+	for (int i = 0; i < n - 1; ++i) 
 	{
-	       std::cout << *it << " ";
+        for (int j = 0; j < n - 1; ++j) 
+		{
+            if (arr[j] > arr[j + 1]) 
+			{
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+            }
+        }
+	}
+
+	std::cout << "Number of the unique elements and result of multiplication of the elements after maximum:\n" << countDistinct(arr, n) << '\t' << res << '\n' << "Sorted array:\n";
+	for (int i = 0; i < n; ++i)
+	{
+		std::cout << arr[i] << ' ';
 	}
 	std::cout << '\n';
 	delete[] arr;
